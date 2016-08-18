1763c12b70aa7443d43117fa06b6eb6087b79ecf
Cláudio Gomes <cla.gms@gmail.com>
2016-08-18 21:42:40 +0200

    running transformation

--- EclipseProjects/MbeddrComponentLanguage/src/transformation/Main.java
+++ EclipseProjects/MbeddrComponentLanguage/src/transformation/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		String projectDir = new File(".").getAbsolutePath();
-		String transformation = new File("./transformation/mbeddr2C_optimized_blinks.dsltrans").getAbsolutePath();		
+		String transformation = new File("./transformation/mbeddr2C_optimized_blinks_nodups.dsltrans").getAbsolutePath();		
 		try {
 			TransformerProcessor tP = new TransformerProcessor(projectDir);
 			tP.LoadModel(transformation);
